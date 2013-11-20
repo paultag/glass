76e32fa017dd746e53231a589cdb5c3ee43f04eb
Paul Tagliamonte <tag@pault.ag>
2013-11-20 00:46:18 -0500

    add more glass objects

--- glass/models.py
+++ glass/models.py
@@ -1,5 +1,8 @@
+import datetime as dt
 import requests
 import json
+import time
+import uuid
 
 
 class GlassAPI(object):
@@ -23,7 +26,7 @@ class GlassAPI(object):
     def post(self, path, **kwargs):
         if 'data' not in kwargs:
             kwargs['data'] = {}
-        data = json.dumps(kwargs.pop('data'))
+        data = json.dumps(kwargs.pop('data'), cls=JSONEncoderPlus)
 
         if 'headers' not in kwargs:
             kwargs['headers'] = {}
@@ -46,12 +49,30 @@ class GlassTimeline(object):
     pass
 
 
+class JSONEncoderPlus(json.JSONEncoder):
+    def default(self, obj, **kwargs):
+        if isinstance(obj, GlassAPIObject):
+            return obj.to_obj()
+        if isinstance(obj, dt.datetime):
+            return time.mktime(obj.utctimetuple())
+        #elif isinstance(obj, dt.date):
+        #    return time.mktime(obj.timetuple())
+        return super(JSONEncoderPlus, self).default(obj, **kwargs)
+
+
 class GlassAPIObject(object):
+    _attrs = {}
+
     def to_obj(self):
-        pass
+        obj = {}
+        for k, v in self._attrs.items():
+            a = getattr(self, k)
+            if a:
+                obj[v] = a
+        return obj
 
     def to_json(self):
-        return json.dumps(self.to_obj())
+        return json.dumps(self.to_obj(), cls=JSONEncoderPlus)
 
 
 class GlassTimelineItemMenu(GlassAPIObject):
@@ -61,6 +82,12 @@ class GlassTimelineItemMenu(GlassAPIObject):
         'OPEN_URI',  # Needs `payload'
         'PLAY_VIDEO',  # Needs `payload'
     ]
+
+    _attrs = {
+        'action': 'action',
+        'id_': 'id',
+        'payload': 'payload',
+    }
 
     def __init__(self, id_, action, payload=None):
         if action not in self.ACTIONS:
@@ -74,15 +101,54 @@ class GlassTimelineItemMenu(GlassAPIObject):
         self.payload = payload
 
 
-    def to_obj(self):
-        o = {
-            "id": self.id_,
-            "action": self.action,
-        }
-        if self.payload:
-            o['payload'] = self.payload
-        return o
-
-
 class GlassTimelineItem(GlassAPIObject):
+    _attrs = {
+        # host: remote
+        'menu_items': 'menuItems',
+        'text': 'text',
+        'html': 'html',
+        'notification': 'notification',
+    }
+
+    def __init__(self, menu_items=None, text=None, html=None):
+        self.menu_items = []
+        if menu_items:
+            self.menu_items = menu_items
+
+        self.text = None
+        self.html = None
+        self.notification = None
+
+        if text and html:
+            raise ValueError("You gave me both Text and HTML. Fail.")
+
+        if text is None and html is None:
+            raise ValueError("You gave me neither HTML nor Text")
+
+        if text:
+            self.text = text
+
+        if html:
+            self.html = html
+
+    def add_menu_item(self, option):
+        self.menu_items.append(option)
+
+
+class NotifiableGlassTimelineItem(GlassTimelineItem):
+    def __init__(self, *args, **kwargs):
+        super(NotifiableGlassTimelineItem, self).__init__(*args, **kwargs)
+        self.notification = {
+            "level": "DEFAULT",  # Only one.
+        }
+
+class DeletableGlassTimelineItem(GlassTimelineItem):
+    def __init__(self, *args, **kwargs):
+        super(DeletableGlassTimelineItem, self).__init__(*args, **kwargs)
+        self.add_menu_item(GlassTimelineItemMenu(id_=str(uuid.uuid4()),
+                                                 action='DELETE'))
+
+class BoringGlassTimelineItem(
+    NotifiableGlassTimelineItem, DeletableGlassTimelineItem
+):
     pass
